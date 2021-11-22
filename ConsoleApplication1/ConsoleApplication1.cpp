5b922bbe7fef4e88f215efacabef5e226e111389
user <user@C1005>
2021-11-22 09:10:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main() {
+
+    while (true) {
+        float n, f=1;
+        cin >> n;
+        if (n < 0)
+            break;
+        for (int i = 1; i <= n; i++)
+            f = f * i;
+        cout << f << endl;
+    }
+    return 0;
+}
